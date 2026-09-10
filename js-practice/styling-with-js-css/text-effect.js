95cd72228c4e459915e2079033f0377e27824602
DESKTOP-G3APRI2\user <houssam.khaled@lau.edu>
2026-09-10 19:50:35 +0300

    styling with js

---
+++ js-practice/styling-with-js-css/text-effect.js
@@ -0,0 +1,46 @@
+
+const textPreview =  document.getElementById("text-preview");
+const customTextInput = document.getElementById("custom-text")
+const classList = document.getElementById("class-list")
+
+function reflectChangesIntoPreview(){
+    setTextPreview(customTextInput.value)
+
+}
+
+function resetAll(){
+    customTextInput.value = "This is sample text"
+    setTextPreview(customTextInput.value)
+
+    const activeButtons = document.querySelectorAll(".effect-button.active")
+    for(let button of activeButtons){
+        button.classList.remove('active')
+    }
+    textPreview.classList = ['text-preview']
+    classList.textContent = "No classes applied"
+}
+
+
+function setTextPreview(text){
+    textPreview.textContent = text
+}
+
+function toggleEffect(id, className){
+    if(textPreview.classList.contains(className)){
+        textPreview.classList.remove(className)
+        document.getElementById(id).classList.remove('active')
+
+    } else {
+
+        textPreview.classList.add(className)
+        document.getElementById(id).classList.add('active')
+    }
+
+    if(textPreview.classList.length === 1){
+        classList.textContent = "No classes applied"
+    } else{
+        classList.textContent = Array.from(textPreview.classList).splice(1)
+    }
+    
+    
+}
